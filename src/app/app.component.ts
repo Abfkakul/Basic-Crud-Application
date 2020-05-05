54de09747f38748474b0cb64fa7a0524e7fa29ad
AbfKakul <anasbinfaisal996@gmail.com>
2020-05-05 17:33:07 +0500

    Importing the Okta service and implementing constructor and login,logout method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyAngularClient';
   public isAuthenticated: boolean;
 
-  constructor() {
-    this.isAuthenticated = false;
+  constructor(public oktaAuth: OktaAuthService) {
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
   }
   
   login() {
+    this.oktaAuth.loginRedirect();
+  }
+  
+  logout() {
+    this.oktaAuth.logout('/');
   }
 
-  logout() {
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
   }
 }
