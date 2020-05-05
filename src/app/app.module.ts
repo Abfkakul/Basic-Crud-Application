d462796b77944af30aeeb43a14b7e65a7747664a
AbfKakul <anasbinfaisal996@gmail.com>
2020-05-05 17:29:31 +0500

    importing OktaAuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
+import { OktaAuthModule } from '@okta/okta-angular';
+
 
 @NgModule({
   declarations: [
@@ -39,6 +41,11 @@ import { HomeComponent } from './home/home.component';
     MatDividerModule,
     MatProgressSpinnerModule,
     FormsModule,
+    OktaAuthModule.initAuth({
+      issuer: 'https://dev-149159.okta.com/oauth2/default',
+      redirectUri: 'http://localhost:4200/implicit/callback',
+      clientId: '0oaaznzomJxLaZIch4x6'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
